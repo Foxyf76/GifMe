22474cda4afd8fb5d646f675ae439f7fe4ddf6f9
Luke Fox <lukefox5@gmail.com>
2020-11-24 20:26:00 +0000

    Fixed static build path

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client'));
+  // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
